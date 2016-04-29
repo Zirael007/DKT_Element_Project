d8882d51c3ae271b86192f579d75579784f998d3
Ganesh <me12b1041@iith.ac.in>
2016-04-29 13:18:53 +0530

    Praetor

--- Quad_shape_fxn.m
+++ Quad_shape_fxn.m
@@ -1,12 +1,14 @@
-function N =  Quad_shape_fxn(x, y)
+function N =  Quad_shape_fxn(xi, yi)
 
     % Shape functions for Quad element
     % 8 noded element (4 corners and 4 mid nodes)
 
-    N(1) = *(1 - x -y)*(0.5 - x - y);
-    N(2) = x*(2*x - 1);
-    N(3) = y*(2*y - 1);
-    N(4) = 4*x*y;
-    N(5) = 4*y*(1 - x - y);
-    N(6) = 4*x*(1 - x - y);
+    N(1) = -0.25*((1-xi)*(1-yi)*(1+xi+yi));
+    N(2) = -0.25*((1+xi)*(1-yi)*(1-xi+yi));
+    N(3) = -0.25*((1+xi)*(1+yi)*(1-xi-yi));
+    N(4) = -0.25*((1-xi)*(1+yi)*(1+xi-yi));
+    N(5) = 0.5*(1-xi^2)*(1-yi);
+    N(6) = 0.5*(1+xi)*(1-yi^2);
+    N(7) = 0.5*(1-xi^2)*(1+yi);
+    N(8) = 0.5*(1-xi^2)*(1-yi^2);
 end
--- Tri_shape_fxn.m
+++ Tri_shape_fxn.m
@@ -1,13 +1,13 @@
-function N =  Tri_shape_fxn(x, y)
+function N =  Tri_shape_fxn(xi, yi)
 
     % Shape functions for Triangular element
     % 6 noded element (3 corners and 3 mid nodes)
 
-    N(1) = 2*(1 - x -y)*(0.5 - x - y);
-    N(2) = x*(2*x - 1);
-    N(3) = y*(2*y - 1);
-    N(4) = 4*x*y;
-    N(5) = 4*y*(1 - x - y);
-    N(6) = 4*x*(1 - x - y);
+    N(1) = 2*(1 - xi -yi)*(0.5 - xi - yi);
+    N(2) = xi*(2*xi - 1);
+    N(3) = yi*(2*yi - 1);
+    N(4) = 4*xi*yi;
+    N(5) = 4*yi*(1 - xi - yi);
+    N(6) = 4*xi*(1 - xi - yi);
 
 end
