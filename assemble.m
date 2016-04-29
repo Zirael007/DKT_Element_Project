c4bfa13e5ddfa5d52f604805954e9a14bad68844
Ganesh <me12b1041@iith.ac.in>
2016-04-29 13:41:35 +0530

    Praetor

---
+++ assemble.m
@@ -0,0 +1,16 @@
+function [kk,ff]=assemble(kk,ff,k,f,index)
+%----------------------------------------------------------
+%  
+%-----------------------------------------------------------
+
+ 
+ ele_dof = length(index);
+ for i=1:ele_dof
+   ii=index(i);
+     ff(ii)=ff(ii)+f(i);
+     for j=1:ele_dof
+       jj=index(j);
+         kk(ii,jj)=kk(ii,jj)+k(i,j);
+     end
+ end
+
---
+++ elementdof.m
@@ -0,0 +1,16 @@
+function [index]=elementdof(node,nnel,ndof)
+%----------------------------------------------------------
+%  
+%-----------------------------------------------------------
+ 
+ 
+   k=0;
+   for i=1:nnel
+     start = (node(i)-1)*ndof;
+       for j=1:ndof
+         k=k+1;
+         index(k)=start+j;
+       end
+   end
+
+ 
